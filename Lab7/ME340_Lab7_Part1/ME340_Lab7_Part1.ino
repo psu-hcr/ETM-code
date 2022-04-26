f12140f45e3089e653bc65eb4d02c869d0257684
zxl5344 <zxl5344@gmail.com>
2022-04-26 15:33:59 -0400

    fix format

--- Lab7/ME340_Lab7_Part1/ME340_Lab7_Part1.ino
+++ Lab7/ME340_Lab7_Part1/ME340_Lab7_Part1.ino
@@ -1,4 +1,4 @@
-eint phototransD = 9; //the digital input pin
+int phototransD = 9; //the digital input pin
 int phototransA = A0;//the analog input pin
 // decrlaring variables to store the input values
 int val =0;
