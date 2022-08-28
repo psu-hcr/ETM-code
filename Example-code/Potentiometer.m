e46b441d747968ce0bd829a917fe61ad5a8a9fff
zxl5344 <zxl5344@gmail.com>
2022-08-28 16:30:22 -0400

    add comment to potentiometer

--- Example-code/Potentiometer.m
+++ Example-code/Potentiometer.m
@@ -1,14 +1,14 @@
-close all;                          % close all window
-clear all;                          % clear all variable
-clc;                                % clear command window
+close all;                              % close all window
+clear all;                              % clear all variable
+clc;                                    % clear command window
 
 %create an Arduino board object
 a = arduino();
 
 time = 200;
 while time > 0
-    voltage = readVoltage(a, 'A0');
-    writePWMVoltage(a, 'D11', voltage);
-    time = time - 1;
-    pause(0.1);
+    voltage = readVoltage(a, 'A0');     % read voltage at pin A0
+    writePWMVoltage(a, 'D11', voltage); % wirte same voltage at pin 11
+    time = time - 1;                    % reduce timer
+    pause(0.1);                         % keep current pin voltage for 0.1s
 end
